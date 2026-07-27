ae5d7d12d086286ede2fe3c200303c0f548fd314
Anupama Maurya <mauryanupama1411@gmail.com>
2026-07-27 09:23:07 +0530

    Update frontend API URL for production

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,4 +1,6 @@
-export const USER_API_END_POINT = "https://job-portal-da8f.onrender.com/api/v1/user";
-export const JOB_API_END_POINT = "http://job-portal-da8f.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT = "https://job-portal-da8f.onrender.com/api/v1/applicantion";
- export const COMPANY_API_END_POINT="https://job-portal-da8f.onrender.com/api/v1/company";
+const API = import.meta.env.VITE_API_URL;
+
+export const USER_API_END_POINT = `${API}/user`;
+export const JOB_API_END_POINT = `${API}/job`;
+export const APPLICATION_API_END_POINT = `${API}/applicantion`;
+export const COMPANY_API_END_POINT = `${API}/company`;
