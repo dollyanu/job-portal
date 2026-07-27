01684864be85ea7cafaf98b4de5b1c36db9bab17
Anupama Maurya <mauryanupama1411@gmail.com>
2026-07-27 08:35:41 +0530

    Fix utils import paths

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import connectDB from "./Utils/db.js";
+import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
